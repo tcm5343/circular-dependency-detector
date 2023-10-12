eebaa8374a03826530a56b9c30c37f545f13d1b2
tcm5343 <gunstuff1@tutanota.com>
2023-10-12 17:51:13 -0500

    ls dirs

--- pkg/main.go
+++ pkg/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 	fmt.Println("Howdy Partner!")
+	entries, err := os.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, e := range entries {
+		fmt.Println(e.Name())
+	}
 }
