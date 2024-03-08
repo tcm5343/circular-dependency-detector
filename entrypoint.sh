5e6531e8aae3b2b77a0ade70811ccb5d454dfd47
tcm5343 <gunstuff1@tutanota.com>
2024-03-08 16:26:56 -0600

    fix local too

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,5 @@ set -e
 
 INPUT_FILE=$1
 
-ls
-ls ./testing/data
-pwd
-
-# build source code
-# CGO_ENABLED=0 GOOS=linux go build -o /app/circular-dependency-detector
-
 /app/circular-dependency-detector $INPUT_FILE
 # python3 ./visualizer/visualize.py
